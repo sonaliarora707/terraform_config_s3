bf7c651e3853216f218355c5872f4bd5b661a78e
Sonali Arora <sonaliarora2012@icloud.com>
2023-02-16 00:27:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+# resource "aws_s3_bucket" "b" {
+#   bucket = "my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "example" {
+#   bucket = aws_s3_bucket.b.id
+#   acl    = "private"
+# }
+
+# Configure the AWS Provider
+  
+variable "s3_bucket_names" {
+  type = list
+  default = ["dev-bucket.app", "uat-bucket.app", "prod-bucket.app"]
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "aws_s3_bucket" "rugged_buckets" {
+  count         = length(var.s3_bucket_names) //count will be 3
+  bucket        = var.s3_bucket_names[count.index]
+  acl           = "private"
+  region        = "us-east-1"
+  force_destroy = true
+  tags = {
+  Owner = "stackguardian"
+  }
 }
